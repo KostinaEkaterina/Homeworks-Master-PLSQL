3def3006d0e178af9839583c8ad9cd04e2b632de
Костина Екатерина <katyakostina22071995@gmail.com>
2025-04-14 22:39:56 +0500

    Отредактируем отступы

--- mkdir hw3/hw3.sql
+++ mkdir hw3/hw3.sql
@@ -8,7 +8,7 @@ declare
 	v_message varchar2(200) := 'Платеж создан';
 	c_new_status constant number := 0;
 begin
-  dbms_output.put_line (v_message || '. Статус: ' || c_new_status);
+	dbms_output.put_line (v_message || '. Статус: ' || c_new_status);
 end;
 /
 -- Перевод платежа в ошибочный статус с описанием причины
@@ -17,7 +17,7 @@ declare
 	v_reason varchar2(200) := 'недостаточно средств';
 	c_error_status constant number := 2;
 begin
-  dbms_output.put_line (v_message || '. Статус: '|| c_error_status ||'. Причина: ' || v_reason);
+	dbms_output.put_line (v_message || '. Статус: '|| c_error_status ||'. Причина: ' || v_reason);
 end;
 /
 -- Отмена платежа
@@ -26,7 +26,7 @@ declare
 	v_reason varchar2(200) := 'ошибка пользователя';
 	c_cancel_status constant number := 3;
 begin
-  dbms_output.put_line (v_message || '. Статус: ' || c_cancel_status|| '. Причина: ' || v_reason);
+	dbms_output.put_line (v_message || '. Статус: ' || c_cancel_status|| '. Причина: ' || v_reason);
 end;
 /
 -- Завершение платежа
@@ -34,20 +34,20 @@ declare
 	v_message varchar2(200) := 'Успешное завершение платежа';
 	c_success_status constant number := 1;
 begin
-  dbms_output.put_line (v_message || '. Статус: ' || c_success_status);
+	dbms_output.put_line (v_message || '. Статус: ' || c_success_status);
 end;
 /
 -- Добавление или обновление данных платежа
 declare
 	v_message varchar2(200) := 'Данные платежа добавлены или обновлены по списку';
 begin
-  dbms_output.put_line (v_message || ' id_поля/значение');
+	dbms_output.put_line (v_message || ' id_поля/значение');
 end;
 /
 -- Удаление деталей платежа
 declare
 	v_message varchar2(200) := 'Детали платежа удалены по списку';
 begin
-  dbms_output.put_line (v_message || ' id_полей');
+	dbms_output.put_line (v_message || ' id_полей');
 end;
 /
